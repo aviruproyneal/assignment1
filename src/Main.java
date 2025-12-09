092934e0d58261f4be67e5f0beb768ad606907ec
Avirup Roy <learnneal@gmail.com>
2025-12-09 09:12:36 +0500

    Add initial Main.java with basic code

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print a welcome message
+        System.out.println("Hello, GitHub!");
+
+        // Example: simple addition
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+        // Example: loop
+        System.out.println("Counting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
